a5f158e8d92eac59068e16c785fb9d0cf0aa48be
苗菁 <miaojing@pashanhoo.com>
2023-05-29 11:25:52 +0800

    fix: user select bug

--- src/axios.ts
+++ src/axios.ts
@@ -4,8 +4,7 @@ const instance = axios.create({
   baseURL: 'http://192.168.2.235',
   timeout: 300000,
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer ATT-2-JnSCW2I8fMPu-sK6mAVDZaoEnaHiJkdh'
+    'Content-Type': 'application/json'
   }
 })
 
