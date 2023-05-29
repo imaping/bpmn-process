c3ffb6f336c1e645986c0da216a44485c08cbb74
苗菁 <miaojing@pashanhoo.com>
2023-05-29 11:30:26 +0800

    fix: user select bug

--- src/axios.ts
+++ src/axios.ts
@@ -1,11 +1,24 @@
 import axios from 'axios'
-// 创建一个axios实例
-const instance = axios.create({
-  baseURL: 'http://192.168.2.235',
-  timeout: 300000,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
+
+let instance
+if (process.env.NODE_ENV == 'production') {
+  // 创建一个axios实例
+  instance = axios.create({
+    timeout: 300000,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+} else {
+  // 创建一个axios实例
+  instance = axios.create({
+    baseURL: 'http://192.168.2.235',
+    timeout: 300000,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ATT-2-JnSCW2I8fMPu-sK6mAVDZaoEnaHiJkdh'
+    }
+  })
+}
 
 export default instance
