9068779c8ab9d799d143c31060b73a796e371eae
苗菁 <miaojing@pashanhoo.com>
2023-07-24 15:19:18 +0800

    fix: 删除任务环节默认退回到任意环节

--- src/additional-functions/EnhancementUserTask.ts
+++ src/additional-functions/EnhancementUserTask.ts
@@ -18,12 +18,12 @@ export default function (modeler: Modeler) {
   modeler.on('shape.added', 2000, (event: TaskEvent) => {
     const businessObject = event.element?.businessObject
     if (isUserTask(businessObject)) {
-      const editor = editorStore()
-      const prefix = editor.getProcessEngine
-      if (!businessObject.get(`${prefix}:buttons`)) {
-        businessObject.set(`${prefix}:backType`, '3')
-        businessObject.set(`${prefix}:buttons`, 'rollback')
-      }
+      // const editor = editorStore()
+      // const prefix = editor.getProcessEngine
+      // if (!businessObject.get(`${prefix}:buttons`)) {
+      //   businessObject.set(`${prefix}:backType`, '3')
+      //   businessObject.set(`${prefix}:buttons`, 'rollback')
+      // }
       if (businessObject.name === undefined || businessObject.name === '') {
         businessObject.name = '用户任务 ' + userTaskNum++
       }
